8c4a80b1d73998743815b3ae5f815d11f4da91c2
Jeison 🌱 <jeysonm021@gmail.com>
2024-12-21 15:19:58 -0500

    Chapter 3

--- maps/main.go
+++ maps/main.go
@@ -1,11 +1,22 @@
 package main
 
 // We will be modifying this later
-func CreateMap() {}
+func CreateMap(key1, key2, val1, val2 string) map[string]string{
+	return map[string]string{
+		key1: val1,
+		key2: val2,
+	}
+}
 
-func AddToMap() {}
+func AddToMap(myMap map[string]string, key, val string) map[string]string{
+	myMap[key] = val
+	return myMap
+}
 
-func DeleteFromMap() {}
+func DeleteFromMap(myMap map[string]string, key string) map[string]string{
+	delete(myMap, key)
+	return myMap
+}
 
 // Where we will run our code
 func main() {}
