1a2bbc11acdb65e76ffba79269e19fdbc131f6fe
Jeison 🌱 <jeysonm021@gmail.com>
2024-12-21 13:36:01 -0500

    Chapter 2

--- slices/main.go
+++ slices/main.go
@@ -1,11 +1,37 @@
 package main
 
+import (
+	"fmt"
+	"slices"
+)
+
 // We will be modifying this later
-func CreateSlice() {}
+func CreateSlice() []string{
+	return []string{"dog","cat","monkey"}
+}
 
-func ModifySlice() {}
+func ModifySlice(sl []string) []string {
+        if len(sl) < 3 {
+                return sl
+        }
+        // Create a new slice with a copy of the original elements
+        newSlice := make([]string, len(sl))
+        copy(newSlice, sl) 
 
-func PopSliceValue() {}
+        newSlice[1] = "❤️"
+        return newSlice
+}
+
+func PopSliceValue(sl []string) []string{
+	 return slices.Delete(sl, 2,3)
+	 
+}
 
 // Where we will run our code
-func main() {}
+func main() {
+		originalSlice := CreateSlice() // Create the initial slice
+        modifiedSlice := ModifySlice(originalSlice) // Create a modified copy
+
+        fmt.Println("Original Slice:", originalSlice)  // Output: [dog cat monkey]
+        fmt.Println("Modified Slice:", modifiedSlice) // Output: [dog ❤️ monkey]
+}
