0d19becd5c28360f103badbf337021fd189493ae
Jeison 🌱 <jeysonm021@gmail.com>
2024-12-21 12:50:43 -0500

    Chapter 1

--- variables/main.go
+++ variables/main.go
@@ -1,11 +1,26 @@
 package main
 
+import "fmt"
+
 // We will be modifying this later
-func VariableDeclaration() {}
+func VariableDeclaration() string{
+	return "🐛"
+}
 
-func StringConcatenation() {}
+func StringConcatenation(a, b string) string {
+	return a + b
+}
 
-func IncrementInt() {}
+func IncrementInt(a int) int {
+	return a + 1
+}
 
 // Where we will run our code
-func main() {}
+func main() {
+	variablesDeclaration := VariableDeclaration()
+	strContat := StringConcatenation("⭐", "👀")
+	incInt := IncrementInt(2)
+	fmt.Println(variablesDeclaration)
+	fmt.Println(strContat)
+	fmt.Println(incInt)
+}
