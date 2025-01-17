d5137e0b3b9fe293d77078ad4280102ec253ba33
Jeison 🌱 <jeysonm021@gmail.com>
2025-01-17 07:54:01 -0500

    context, signals

--- context/main.go
+++ context/main.go
@@ -13,11 +13,11 @@ func hello(w http.ResponseWriter, req *http.Request) {
 
 	select {
 	case <-time.After(10 * time.Second):
-		fmt.Fprintf(w, "hello\n")
+		fmt.Fprintf(w, "hello 💀 \n ❤️ ")
 	case <-ctx.Done():
 
 		err := ctx.Err()
-		fmt.Println("server:", err)
+		fmt.Println("server: ☠️ ", err)
 		internalError := http.StatusInternalServerError
 		http.Error(w, err.Error(), internalError)
 	}
